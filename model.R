62e728f0e99cafe98aa14792038ae50a542c7bb7
Adam Cook <adam.cook@maxar.com>
2020-09-04 11:08:54 -0400

    Add pairwise distance calculation

--- model.R
+++ model.R
@@ -1,54 +1,70 @@
 library(reticulate)
 library(lubridate)
 library(tidyverse)
-library(sazedR)
-library(SimilarityMeasures)
+library(dplyr)
+library(padr)
+library(rdist)
 
-use_python("/usr/local/bin/python3")
-np <-import("numpy")
+library(SimilarityMeasures)  # ts similarity
 
-flights <- c()
-filename <- c()
-for (p in Sys.glob('data/652/*.npz', dirmark = FALSE)[1:2]) {
-  flights <- append(flights, np$load(p))
-  filename <- append(filename, basename(p))
-}
-
+# add timestamp function
 add_timestamp <- function(file_name, rate, series) {
   milliseconds_rate <- 1000/rate
   
   tail_number <- substr(file_name, 1,3)
+
   year <- substr(file_name, 4,7)
   month <- substr(file_name, 8,9)
   day <- substr(file_name, 10,11)
   hour <- substr(file_name, 12,13)
   minute <- substr(file_name, 14,15)
-  
   start_time <- mdy_hm(paste(month, "/",day,"/",year, " ", hour,":",minute, sep=""))
-  timestamp <- start_time + milliseconds(sapply(1:nrow(series), function(x) x*milliseconds_rate))
   
+  timestamp <- start_time + milliseconds(sapply(1:nrow(series), function(x) x*milliseconds_rate))
   tibble(timestamp = timestamp, data = series)
 }
 
-rate <- catalog[catalog$X1 == 'ALT',]$rate
-flight1_alt <- add_timestamp(filename[[1]], rate, flights[[1]]$f$ALT)
-flight2_alt <- add_timestamp(filename[[2]], rate, flights[[2]]$f$ALT)
 
-rate <- catalog[catalog$X1 == 'OIP.1',]$rate
-flight1_oip1 <- add_timestamp(filename[[1]], rate, flights[[1]]$f$OIP_1)
-flight2_oip1 <- add_timestamp(filename[[2]], rate, flights[[2]]$f$OIP_1)
+# upse python to load npz files
+use_python("/usr/local/bin/python3")
+np <-import("numpy")
+
+#select flights and feature
+set.seed(42)
+flight_sample_size <- 10
+data_sample_rate = '1 min'
+flights <- list()
+for (p in sample(Sys.glob('data/652/*.npz'), 10)) {
+  flight <- np$load(p)
+  filename <- basename(p)
+
+  rate <- catalog[catalog$X1 == 'OIP.1',]$rate
+  flight <- add_timestamp(filename, rate, flight$f$OIP_1) %>%
+    thicken(data_sample_rate, colname = 'group') %>%
+    group_by(group) %>%
+    summarize(data = mean(data), .groups='keep')
+
+  flights <- append(flights, list(flight))
+}
+remove(p, flight, filename)
 
 
+# compute pairwise distance matrix
+distances = matrix(nrow = length(flights), ncol = length(flights))
+grid_indicies <- expand.grid(a=1:length(flights), b=1:length(flights))
+for (i in 1:nrow(grid_indicies)) {
+  ai <- grid_indicies[i, 'a']
+  bi <- grid_indicies[i, 'b']
 
-ggplot() +
-  geom_point(data = flight1_oip1, aes(x = timestamp, y = data, group = 1), fill = NA, color = "red") +
-  geom_point(data = flight2_oip1, aes(x = timestamp, y = data, group = 1), fill = NA, color = "blue")
-  #geom_polygon(data = usa, aes(x=long, y = lat, group = group), fill = NA, color = "blue")
+  if (ai <= bi) next
 
+  a <- flights[[ai]]
+  b <- flights[[bi]]
 
+  distance <- Frechet(matrix(c(1:nrow(a), a$data), nrow(a)),
+                      matrix(c(1:nrow(b), b$data), nrow(b)))
 
-ggplot(economics, aes(x=timestamp)) + 
-  geom_line(aes(y = psavert), color = "darkred") + 
-  geom_line(aes(y = uempmed), color="steelblue", linetype="twodash")
-
-LCSS(matrix(flight1_oip1$data), matrix(flight2_oip1$data))
+  distances[ai, bi] <- distance
+}
+remove(a, b, i, ai, bi, distance)
+distances
